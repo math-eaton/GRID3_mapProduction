e983bec3266d608ae5ae93c06b6bfaa8ebdc9b77
math-eaton <mheaton@ciesin.columbia.edu>
2023-09-26 23:55:18 -0400

    compress subfolders in dir

--- output_processing/moveToSub_lvl1.py
+++ output_processing/moveToSub_lvl1.py
@@ -9,7 +9,7 @@ pdf_files = [file for file in os.listdir(folder) if file.lower().endswith('.pdf'
 # Iterate through each PDF file and organize them into subfolders
 for pdf_file in pdf_files:
     # Extract the target folder name based on the delimiter ('_') ... adjust the index value based on intended split level
-    target = pdf_file.split('_')[3]
+    target = pdf_file.split('_')[4]
     
     # Create the target directory if it doesn't exist
     target_directory = os.path.join(folder, target)
