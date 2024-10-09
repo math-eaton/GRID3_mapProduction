392f54f963c9cc81a30f2bdbb327056d83a05a25
math-eaton <matthew.j.heaton@gmail.com>
2024-10-09 00:01:48 -0400

    add poly to line for vis

--- preprocess/addPageName_andJoinToPts.py
+++ preprocess/addPageName_andJoinToPts.py
@@ -2,29 +2,41 @@ import arcpy
 from arcpy import env
 import re
 
+
+
 # Set environment settings
-env.workspace = r"E:\mheaton\cartography\COD_KasaiOriental_microplanning_20240820\data\input\GRID3_COD_KO_consolidated_20240711.gdb"
+env.workspace = r"E:\mheaton\cartography\COD_Tschopo-Mongala_microplanning_20240918\RDC_Tschopo-Mongala_aireSante_20240918\DATA\TP_MG_microplanning_20240925.gdb"
 env.overwriteOutput = True
 
+# Set the target geodatabase for output feature classes
+target_gdb = r"E:\mheaton\cartography\COD_Tschopo-Mongala_microplanning_20240918\RDC_Tschopo-Mongala_aireSante_20240918\DATA\TP_MG_microplanning_pageIndex_20240925.gdb"
+
 # Set the local variables
-joinFeatures = "GRID3_COD_KO_Health_areas_consolidation_20240711"
-admin_fields = ["province", "zonesante", "airesante"]
-pagename_fields = ["pagename_admin1", "pagename_admin2", "pagename_admin3"]
+joinFeatures = "TP_MG_healthAreas_merge_20240919"
+admin_fields = ["province", "antenne" ,"zonesante", "airesante"]
+
+# Dynamically generate pagename fields based on admin_fields
+pagename_fields = [f"pagename_{admin}" for admin in admin_fields]
 
 # Optional processing flags
-perform_spatial_join = True  # Set this to False to skip pageName join to point fcs
-perform_dissolve = True  # Set this to False to skip output admin dissolve post-process
+perform_spatial_join = True  # Set this to False if you don't want to perform the spatial join
+perform_dissolve = True  # Set this to False if you don't want to perform the dissolve operation
+perform_polygon_to_line = True  # Set this to False if you don't want to convert polygons to lines
 
-# Ensure the fields do not already exist to prevent an error
+# Ensure only the dynamically named fields are created
 existing_fields = [f.name for f in arcpy.ListFields(joinFeatures)]
 for pagename_field in pagename_fields:
     if pagename_field not in existing_fields:
         arcpy.AddField_management(joinFeatures, pagename_field, "TEXT")
 
 # Create pagename fields for each administrative level
-arcpy.CalculateField_management(joinFeatures, pagename_fields[0], "'RDC_' + !{}!".format(admin_fields[0]), "PYTHON3")
-arcpy.CalculateField_management(joinFeatures, pagename_fields[1], "'RDC_' + !{}! + '_' + !{}!".format(admin_fields[0], admin_fields[1]), "PYTHON3")
-arcpy.CalculateField_management(joinFeatures, pagename_fields[2], "'RDC_' + !{}! + '_' + !{}! + '_' + !{}!".format(admin_fields[0], admin_fields[1], admin_fields[2]), "PYTHON3")
+for i, pagename_field in enumerate(pagename_fields):
+    if i == 0:
+        expression = f"'RDC_' + !{admin_fields[i]}!"
+    else:
+        expression = "'RDC_' + " + " + '_' + ".join([f'!{admin_fields[j]}!' for j in range(i+1)])
+    
+    arcpy.CalculateField_management(joinFeatures, pagename_field, expression, "PYTHON3")
 
 # Define a function to remove non-ascii characters and replace spaces, hyphens, slashes, and other problematic characters
 code_block = """
@@ -50,22 +62,41 @@ if perform_spatial_join:
     for targetFeatures in featureclasses:
         # Exclude the original joinFeatures from the loop
         if targetFeatures != joinFeatures:
-            outfc = f"{targetFeatures}_pagename"
+            outfc = f"{target_gdb}\\{targetFeatures}_pagename"
 
             # Use the Spatial Join tool to join the point feature class with the polygon feature class
             arcpy.analysis.SpatialJoin(targetFeatures, joinFeatures, outfc, join_type="KEEP_COMMON")
 
 # Optional post-processing: Dissolve each pagename_[admin] into a new output feature class
 if perform_dissolve:
-    for pagename_field in pagename_fields:
-        dissolve_output = f"{joinFeatures}_{pagename_field}_dissolved"
+    for i, pagename_field in enumerate(pagename_fields):
+        dissolve_output = f"{target_gdb}\\{joinFeatures}_{pagename_field}_dissolved"
+        
+        # Preserve original admin fields in the dissolve
+        dissolve_fields = [f"FIRST_{admin_fields[j]}" for j in range(i+1)]
+        dissolve_stats = [[admin_fields[j], "FIRST"] for j in range(i+1)]
 
         # Perform the dissolve operation
-        arcpy.management.Dissolve(joinFeatures, dissolve_output, pagename_field, multi_part="SINGLE_PART")
+        arcpy.management.Dissolve(joinFeatures, dissolve_output, pagename_field, dissolve_stats, multi_part="SINGLE_PART")
+
+        # Rename the output fields to match the original admin field names
+        for j, admin_field in enumerate(admin_fields[:i+1]):
+            arcpy.management.AlterField(dissolve_output, f"FIRST_{admin_field}", admin_field)
 
         # Check if the dissolve was successful and the output exists
         if arcpy.Exists(dissolve_output):
             print(f"Dissolved output created: {dissolve_output}")
+            
+            # Optional: Convert the dissolved polygon to a line feature class
+            if perform_polygon_to_line:
+                line_output = f"{dissolve_output}_lineFC"
+                arcpy.management.PolygonToLine(dissolve_output, line_output)
+
+                if arcpy.Exists(line_output):
+                    print(f"Polygon to line output created: {line_output}")
+                else:
+                    print(f"Failed to create polygon to line output: {line_output}")
+
         else:
             print(f"Failed to create dissolved output: {dissolve_output}")
 
