d4849022febdad6884756a8e148698aee69a01da
math-eaton <mheaton@ciesin.columbia.edu>
2025-01-22 16:01:22 -0500

    init astro site

---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'GRID3 Cartographic Standards Manual',
+			logo: {
+				src: './src/assets/logo.png',
+				replacesTitle: false,
+			},
+			social: {
+				github: 'https://github.com/math-eaton/GRID3_mapProduction',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
