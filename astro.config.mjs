7b8e70f3c9bc99968fbb2ca88e0b0db42a7123fc
math-eaton <mheaton@ciesin.columbia.edu>
2025-01-29 17:51:40 -0500

    codeblocks + org

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,62 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://math-eaton.github.io',
-	base: '/GRID3_mapProduction',
-	integrations: [
-		starlight({
-			title: 'GRID3 Cartographic Standards Manual',
-			logo: {
-				src: './src/assets/logo.png',
-				replacesTitle: false,
-			},
-			social: {
-				github: 'https://github.com/math-eaton/GRID3_mapProduction',
-			},
-			sidebar: [
-				{
-					label: 'Manual',
-					autogenerate: { directory: '01-manual' },
-				},
-				{
-					label: 'Style guide',
-					autogenerate: { directory: '02-style' },
-				},
-				{
-					label: 'Tutorials',
-					autogenerate: { directory: '03-demos' },
-				},
-				{
-					label: 'Appendix',
-					autogenerate: { directory: '04-appendices' },
-				},
-			],
+    site: 'https://math-eaton.github.io',
+    base: '/GRID3_mapProduction',
+    integrations: [starlight({
+        title: 'GRID3 Cartographic Standards Manual',
+        logo: {
+            src: './src/assets/logo.png',
+            replacesTitle: false,
+        },
+		customCss: [
+			'./src/styles/tailwind.css',
+			// '@fontsource/fontsource/lato',
+		  ],	
+        social: {
+            github: 'https://github.com/math-eaton/GRID3_mapProduction',
+        },
+        sidebar: [
+            {
+                label: 'Manual',
+                autogenerate: { directory: '01-manual' },
+            },
+            {
+                label: 'Style guide',
+                autogenerate: { directory: '02-style' },
+            },
+            {
+                label: 'Tutorials',
+                autogenerate: { directory: '03-demos' },
+            },
+            {
+                label: 'Appendix',
+                autogenerate: { directory: '04-appendices' },
+                collapsed: true,
+            },
+            { 
+                label: 'GRID3 data',
+                link: 'https://data.grid3.org/',
+                // badge: { text: 'External', variant: 'caution' },
+            },
+            { 
+                label: 'GitHub',
+                link: 'https://github.com/math-eaton/GRID3_mapProduction/tree/main/scripts/', 
+                // badge: { text: 'External', variant: 'caution' },
+            },
+
+
+
+        ],
+	}),     
+	tailwind({
+		// Disable the default base styles:
+		applyBaseStyles: false,
 		}),
 	],
 });
+
